a8c5441dbafaab5bb1ffa6bc7d5eb333902b168e
Jim Stewart <jim@stewartstuff.com>
2013-01-30 08:04:15 -0600

    added missing files

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios
+target :SplitViewToyTests, :exclusive => true do
+    pod 'Kiwi'
+end
---
+++ Pods/PodsDummy_Pods_SplitViewToyTests.m
@@ -0,0 +1,4 @@
+@interface PodsDummy_Pods_SplitViewToyTests : NSObject
+@end
+@implementation PodsDummy_Pods_SplitViewToyTests
+@end
